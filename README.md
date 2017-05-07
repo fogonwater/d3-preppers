c42f139c865f3ed1bdaa97384aa0e93bde739b00
Chris McDowall <chris.mcdowall@gmail.com>
2017-05-07 14:40:00 +1200

    added D3graph for prepping d3 networks

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # d3-preppers
-Python functions to help prep data for D3
+Python functions to help prepare data for D3.
+
+
+## Helpful tutorials
+
+- [Understanding D3.js Force Layout - 1: The Simplest Possible Graph](http://bl.ocks.org/sathomas/11550728) (a series)
+- [Mike Bostock’s Les Misérables graph](https://bl.ocks.org/mbostock/4062045)
+- [Basics of d3 force directed graphs](http://www.puzzlr.org/basics-of-d3-force-directed-graphs/)
+- [d3-force: minimal working example](https://bl.ocks.org/puzzler10/4efcb280a23c2f9b824879771ae41592)
---
+++ preppers.py
@@ -0,0 +1,87 @@
+import json
+
+class D3graph:
+    def __init__(self):
+        self.nodes = {}
+        self.links = []
+
+    def upsert_node(self, _id, value=0, static_attributes={}):
+        """
+        Update an existing node, or create a new one. Only the
+        "value" property will be incremented.
+        """
+        nodes = self.nodes
+
+        # If no value supplied, but one appears in static_attributes,
+        # use the static_attributes instead. This is probably a dumb idea.
+        if not value and 'value' in static_attributes:
+            value = float(static_attributes['value'])
+
+        # If no node exists, create a new node one with a value.
+        if not _id in nodes:
+            nodes[_id] = {'id':_id, 'value': value}
+        # Otherwise increment the existing value
+        else:
+            nodes[_id]['value'] += value
+
+        # Use static_attributes to set other node attributes
+        # Overrides existing key:values with most recently received
+        for k, v in static_attributes.items():
+            # Ignore any key named 'value'.
+            if k == 'value':
+                continue
+            nodes[_id][k] = v
+
+    def add_link(self, source, target, value=0, static_attributes={}):
+        """
+        Add a new source -> target link, with optional attributes.
+        Updates nodes along the way, creating new ones if necessary
+        """
+        self.upsert_node(source, value=value)
+        self.upsert_node(target, value=value)
+        base_link = {'source':source,'target':target, 'value': value}
+
+        # remove any dangerous key:values from static_attributes
+        # TODO - this shouldn't happen silently
+        static_attributes.pop("source", None)
+        static_attributes.pop("target", None)
+        static_attributes.pop("value", None)
+
+        # combine base_link and any static_attributes
+        self.links.append( dict(base_link.items() + static_attributes.items()) )
+
+    def export(self, f_path):
+        """ Export the nodes and values to a D3 JSON file """
+        report = {'nodes':self.nodes.values(), 'links':self.links}
+        with open(f_path, 'w') as outfile:
+            json.dump(
+                report,
+                outfile,
+                indent=2,
+                #sort_keys=True
+            )
+        print('{} written.'.format(f_path))
+
+
+def test():
+    g = D3graph()
+    data = [
+        ['c','t',5],
+        ['c','v',5],
+        ['t','v',5],
+        ['v','h',10],
+        ['v','s',10],
+        ['p','c',10],
+        ['b','c',10],
+        ['t','m',10],
+        ['t','j',10],
+    ]
+    for row in data:
+        g.add_link(row[0], row[1], row[2])
+    g.export('test.json')
+
+def main():
+    test()
+
+if __name__ == '__main__':
+    main()
